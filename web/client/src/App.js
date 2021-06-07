ee846285842b11945ae6fcbafc6bde700b226b20
LimEunSeop <dmstjq12@naver.com>
2021-06-07 10:46:33 +0900

    클라이언트 수정

--- web/client/src/App.js
+++ web/client/src/App.js
@@ -1,14 +1,35 @@
 import { useEffect, useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
-  const [state, setstate] = useState(null)
+  const [state, setState] = useState(null)
 
   useEffect(() => {
-    fetch(`/rest/test`)
+    fetch('/rest/test')
       .then((res) => res.text())
-      .then((text) => setstate(text))
+      .then((text) => setState(text))
   }, [])
-  return <div className="App">{state}</div>
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{state}</p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
 }
 
 export default App
